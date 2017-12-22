986d9e95f60aaef5f7ca90fa0ce5a714148e5b74
Michael R. Bagnall <mrbagnall@icloud.com>
2017-12-22 02:33:01 -0600

    Change the document root and pull in our theme and module via git

--- conf/run-httpd.sh
+++ conf/run-httpd.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+mkdir /var/www/html/web/modules/custom
+git clone https://github.com/ElusiveMind/probo-drupal.git /var/www/html/web/modules/custom
+git clone https://github.com/ElusiveMind/jupither.git /var/www/html/web/themes
+
 # Make sure we're not confused by old, incompletely-shutdown httpd
 # context after restarting the container.  httpd won't start correctly
 # if it thinks it is already running.
