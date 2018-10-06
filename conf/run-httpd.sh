2c6e18ae25504a8b349421d7a422324274c6bab1
Michael Bagnall <mrbagnall@icloud.com>
2018-10-05 20:58:57 -0400

    Finalized variables

--- conf/run-httpd.sh
+++ conf/run-httpd.sh
@@ -2,12 +2,13 @@
 
 # Do the base installation of Drupal
 drush -y -r /var/www/html/web si standard \
-  --db-url=mysql://$PROBO_DB_USERNAME:$PROBO_DB_PASSWORD@$PROBO_DB_HOSTNAME/$PROBO_DB_DATABASE \
-  --account-pass="$PROBO_ADMIN_ACCOUNT_PASSWORD" \
-  --account-name="$PROBO_ADMIN_ACCOUNT_USERNAME" \
-  --account-mail="$PROBO_ADMIN_ACCOUNT_EMAIL" \
+  --db-url=mysql://$PORTAL_DB_USERNAME:$PORTAL_DB_PASSWORD@$PORTAL_DB_HOSTNAME/$PORTAL_DB_DATABASE \
+  --account-pass="$PORTAL_ADMIN_ACCOUNT_PASSWORD" \
+  --account-name="$PORTAL_ADMIN_ACCOUNT_USERNAME" \
+  --account-mail="$PORTAL_ADMIN_ACCOUNT_EMAIL" \
   --site-name="Open Source Probo Portal"
 chown -R apache:apache /var/www/html
+
 
 # Make sure we're not confused by old, incompletely-shutdown httpd
 # context after restarting the container.  httpd won't start correctly
