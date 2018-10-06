168906cae91f4a7ba4ecc085ad2c0140e1b124e0
Michael Bagnall <mrbagnall@icloud.com>
2018-10-05 20:03:51 -0400

    remove MySQL commands and fix variables

--- conf/run-httpd.sh
+++ conf/run-httpd.sh
@@ -1,19 +1,11 @@
 #!/bin/bash
 
-# Initialize MariaDB and install our database for use by Drupal.
-#mysql_install_db --user=mysql
-#mysqld_safe --user=mysql &
-#sleep 15
-#mysqladmin -uroot password 'proboci'
-#echo "CREATE DATABASE probo" | mysql -uroot -pproboci
-
 # Do the base installation of Drupal
-sleep 15
 drush -y -r /var/www/html/web si standard \
-  --db-url=mysql://${PROBO_DB_USERNAME}:${PROBO_DB_PASSWORD}@${PROBO_DB_HOSTNAME}/${PROBO_DB_DATABASE} \
-  --account-pass="${PROBO_ADMIN_ACCOUNT_PASSWORD}" \
-  --account-name="${PROBO_ADMIN_ACCOUNT_USERNAME}" \
-  --account-mail="${PROBO_ADMIN_ACCOUNT_EMAIL}" \
+  --db-url=mysql://$PROBO_DB_USERNAME:$PROBO_DB_PASSWORD@$PROBO_DB_HOSTNAME/$PROBO_DB_DATABASE \
+  --account-pass="$PROBO_ADMIN_ACCOUNT_PASSWORD" \
+  --account-name="$PROBO_ADMIN_ACCOUNT_USERNAME" \
+  --account-mail="$PROBO_ADMIN_ACCOUNT_EMAIL" \
   --site-name="Open Source Probo Portal"
 chown -R apache:apache /var/www/html
 
