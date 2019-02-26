4264fbce426966e53c5efa6479859b38b39e3024
Michael Bagnall <mrbagnall@icloud.com>
2019-02-26 10:06:12 -0500

    Putting the build into the shell to see if it works

--- conf/run-httpd.sh
+++ conf/run-httpd.sh
@@ -2,6 +2,15 @@
 
 # Only run our Drupal install if we do not have a Drupal install currently.
 if [ ! -f "/var/www/html/web/sites/default/settings.php" ]; then
+  # Install Drupal via composer and do so as the apache user.
+  rm -rf /var/www/html
+  composer create-project drupal-composer/drupal-project:8.x-dev /var/www/html --stability dev --no-interaction
+  composer --working-dir=/var/www/html require drupal/admin_toolbar:^1.0
+  composer --working-dir=/var/www/html require drupal/module_filter:^3.0
+  composer --working-dir=/var/www/html require drupal/smtp:^1.0
+  composer --working-dir=/var/www/html require drupal/probo:1.x-dev
+  composer --working-dir=/var/www/html require drupal/proboci:1.x-dev
+
   # Do the base installation of Drupal
   drush -y -r /var/www/html/web si standard \
     --db-url=mysql://$PORTAL_DB_USERNAME:$PORTAL_DB_PASSWORD@$PORTAL_DB_HOSTNAME/$PORTAL_DB_DATABASE \
